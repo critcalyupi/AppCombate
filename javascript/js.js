18c046c055091ff406971cca26f22c124e17a538
Iraldo <igonzalezc@iesfbmoll.org>
2019-09-08 10:45:37 +0200

    0.0.0.1 - Alternar entre Pantallas

---
+++ javascript/js.js
@@ -0,0 +1,8 @@
+function entrarJuego(){
+    document.getElementById("entry_window").style.display = "none";
+    document.getElementById("main_window").style.display = "block";
+}
+function salirJuego(){
+    document.getElementById("entry_window").style.display = "block";
+    document.getElementById("main_window").style.display = "none";
+}
