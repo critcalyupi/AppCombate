a321157e56c37b7e8fb94aa5a098a3d6ee22c2f9
Iraldo <igonzalezc@iesfbmoll.org>
2019-09-11 22:08:49 +0200

    0.0.0.4 - Nombres mas logicos, introduccion de clip-paths y demas
    Tocada de huevos con los espacios al pasarlo por scss... Eliminados bordes porque se los comía. Volvemos al darkgray del body. Ajustes en las fichitas. Testeo de Javascripts para el futuro y arreglo en la importación porque soy gilipollas

--- javascript/js.js
+++ javascript/js.js
@@ -6,3 +6,11 @@ function salirJuego(){
     document.getElementById("entry_window").style.display = "block";
     document.getElementById("main_window").style.display = "none";
 }
+/*
+$(document).ready(function(){
+    var x;
+    for (var i=1; i<=12; i++){
+        x = $("#ficha_"+i).offset();
+        $('.ficha_'+i).text("numero "+i+" "+x.top+" "+x.left);
+    }
+});*/
